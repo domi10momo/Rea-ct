8c21a56965fd00604e14ed2e8d7a87015eff1339
Masaki Hisadomi <hb.kmaia93@icloud.com>
2021-05-10 00:46:42 +0900

    カウントアップをhooksで 9-2

---
+++ count-up-hooks/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState, VFC } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Card, Statistic } from "semantic-ui-react";
+
+const Counter: VFC = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount((c) => c + 1);
+  const reset = () => setCount(0);
+
+  return (
+    <Card>
+      <Statistic className="number-board">
+        <Statistic.Label>count</Statistic.Label>
+        <Statistic.Value>{count}</Statistic.Value>
+      </Statistic>
+      <Card.Content>
+        <div className="ui two buttons">
+          <Button color="red" onClick={reset}>
+            Reset
+          </Button>
+          <Button color="green" onClick={increment}>
+            +1
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default Counter;
